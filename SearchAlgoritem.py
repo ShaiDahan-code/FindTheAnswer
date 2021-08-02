9bfc26d75b4b0c19c8ce866d8ea8bb1cf6e1c5f8
sarel <sarel190@gmail.com>
2021-08-02 20:52:44 +0300

    *update the project* fix all the problem - change the sum of the site to 2 (its improve the reliability )

--- SearchAlgoritem.py
+++ SearchAlgoritem.py
@@ -8,12 +8,9 @@ headers = {
 #shai is a gay
 class Search:
 
-    def __init__(self,question, answer_1, answer_2, answer_3, answer_4,):
+    def __init__(self, question, answer_1, answer_2, answer_3, answer_4):
         self.googleURL = "https://www.google.com/search?q=" + question
-        self.poosible_answer_1 = (answer_1,0)
-        self.poosible_answer_2 = (answer_2,0)
-        self.poosible_answer_3 = (answer_3,0)
-        self.poosible_answer_4 = (answer_4,0)
+        self.poosible_answer = [[answer_1,0],[answer_2,0],[answer_3,0],[answer_4,0]]
         self.question = question
         self.listURLS = []
         print("Init suuces!")
@@ -41,32 +38,35 @@ class Search:
             if siteResponse.status_code == 200:
                 text = siteResponse.text.split()
                 for word in text:
-                    if(word in self.poosible_answer_1[0]):
-                        self.poosible_answer_1[1]+=1
-                    elif(word in self.poosible_answer_2[0]):
-                        self.poosible_answer_2[1]+=1
-                    elif (word in self.poosible_answer_3[0]):
-                        self.poosible_answer_3[1] += 1
-                    elif (word in self.poosible_answer_4[0]):
-                        self.poosible_answer_4[1] += 1
+                    if(word in self.poosible_answer[0][0]):
+                        self.poosible_answer[0][1] += 1
+                    elif(word in self.poosible_answer[1][0]):
+                        self.poosible_answer[1][1] += 1
+                    elif (word in self.poosible_answer[2][0]):
+                        self.poosible_answer[2][1] += 1
+                    elif (word in self.poosible_answer[3][0]):
+                        self.poosible_answer[3][1] += 1
             elif siteResponse.status_code == 404:
                 print("Fail connect to Google site!")
-        print(self.poosible_answer_1[1])
+
 
     def searchAnswer(self):
         googleResponse = rq.get(self.googleURL, headers=headers, allow_redirects=True)
         if googleResponse.status_code == 200:
             self.listURLS = list(self.getHTTP(googleResponse.text))
-            self.listURLS = self.listURLS[4:10]
+            self.listURLS = self.listURLS[4:6]
         elif googleResponse.status_code == 404:
             print("Fail connect to Google site!")
 
 
-
-
-
-
-
-
     def getAnswer(self):
-        pass
+        biggest = self.poosible_answer[0][1]
+        key = 0
+        i = 0
+        for countAnswer in self.poosible_answer:
+            print(countAnswer[1])
+            if countAnswer[1] > biggest:
+                biggest = countAnswer[1]
+                key = i
+            i+=1
+        print(self.poosible_answer[key][0])
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import SearchAlgoritem as sa
 
-p1 = sa.Search("הבן אדם המבוגר בעולם","ז'אן לואיז קלמן","bla","bla","bla")
+p1 = sa.Search("הבן אדם המבוגר בעולם","אביחי עזיז","ז'אן לואיז קלמן","שראל אלוש","אסף בן שבת")
 p1.searchAnswer()
 p1.countTheAnswer()
-
+p1.getAnswer()
