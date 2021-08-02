1428bf8a8347aeee1a9855e5f32bfeda7dcd2d1b
Shai Dahan <shai.0.dahan@gmail.com>
2021-08-02 18:43:07 +0300

    Add: getHTTP - from request.text get the website to serach

---
+++ main.py
@@ -0,0 +1,6 @@
+import SearchAlgoritem as sa
+
+p1 = sa.Search("הבן אדם המבוגר בעולם","bla","bla","bla","bla")
+p1.searchAnswer()
+
+
--- venv/SearchAlgoritem.py
+++ venv/SearchAlgoritem.py
@@ -1,11 +1,19 @@
+import requests as rq
+from urllib.parse import urlencode,urlparse,parse_qs
+from lxml.html import fromstring
+
+
 class Search:
     question = ""
     poosible_answer_1 = ""
     poosible_answer_2 = ""
     poosible_answer_3 = ""
     poosible_answer_4 = ""
+    ansewr = ""
+    googleURL = ""
 
     def __init__(self,question, answer_1,answer_2,answer_3,answer_4,):
+        self.googleURL = "https://www.google.com/search?q=" + question
         self.poosible_answer_1 = answer_1
         self.poosible_answer_2 = answer_2
         self.poosible_answer_3 = answer_3
@@ -13,7 +21,44 @@ class Search:
         self.question = question
         print("Init suuces!")
 
-    def searchAnswer(self):
-        pass
 
-    def returnAnswer(self):
+    def getHTTP(self , text):
+        print()
+        listURLS = []
+        new_word = ""
+        for word in text.split('href="'):
+            if(word.startswith("http")):
+                for i in word:
+                    if i != '"':
+                        new_word += i
+                    else:
+                        break
+                listURLS.append(new_word)
+                new_word = ""
+        for i in listURLS:
+            print(i)
+
+
+    def searchAnswer(self):
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36'
+        }
+        googleResponse = rq.get(self.googleURL, headers=headers, allow_redirects=True)
+        if googleResponse.status_code == 200:
+
+            print(googleResponse.text)
+            self.getHTTP(googleResponse.text)
+
+            print("Enter to Google site!")
+        elif googleResponse.status_code == 404:
+            print("Fail connect to Google site!")
+
+
+
+
+
+
+
+
+    def getAnswer(self):
+        return answer
