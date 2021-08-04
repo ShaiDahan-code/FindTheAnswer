d68e6155b0c361afb59c6a81e50231d07a7d7cae
Shai Dahan <shai.0.dahan@gmail.com>
2021-08-04 17:52:57 +0300

    ImageToText.py : add option to add question from photo

--- GUI.py
+++ GUI.py
@@ -1,5 +1,6 @@
 import PySimpleGUI as sg
 import SearchAlgoritem
+import ImageToText
 
 layout = [  [sg.Text('Question:                          '), sg.Input(key='Question')],
             [sg.Text('Possible answer number 1: '), sg.Input(key='Answer1')],
@@ -7,7 +8,7 @@ layout = [  [sg.Text('Question:                          '), sg.Input(key='Quest
             [sg.Text('Possible answer number 3: '), sg.Input(key='Answer3')],
             [sg.Text('Possible answer number 4: '), sg.Input(key='Answer4')],
             [sg.Text("Answer is: "), sg.Text(size=(70, 1), key='-OUTPUT-')],
-            [sg.Button('Search Answer'), sg.Button('Clear'),sg.Button('Quit'), sg.Text("Created by: Sarel Alush & Shai Dahan")]
+            [sg.Button('Search Answer'), sg.Button('Clear'), sg.Button('Upload Image'), sg.Button('Quit'), sg.Text("Created by: Sarel Alush & Shai Dahan")]
 ]
 window = sg.Window('Find The Answer', layout)
 while True:
@@ -23,15 +24,20 @@ while True:
         window['Answer4'].update("")
         window['-OUTPUT-'].update("")
 
+    if event == 'Upload Image':
+        p2 = ImageToText.ImageToTextConvert()
+
+
+
     if event == 'Search Answer':
         p1 = SearchAlgoritem.Search(values['Question'], values['Answer1'], values['Answer2'], values['Answer3'], values['Answer4'])
         p1.searchAnswer()
         p1.countTheAnswer()
-        if(len(p1.getAnswer()) == 4):
+        if len(p1.getAnswer()) == 4:
             window['-OUTPUT-'].update(f"The most possible answer is: FAIL TO FIND ")
-        elif(len(p1.getAnswer()) == 1):
+        elif len(p1.getAnswer()) == 1:
             window['-OUTPUT-'].update(f"The most possible answer is: {p1.getAnswer()[0][0]} ({int(p1.getPercent())}%)")
-        elif (len(p1.getAnswer()) > 1):
+        elif len(p1.getAnswer()) > 1:
             window['-OUTPUT-'].update(f"The most possible answer is one from them:{[i[0] for i in p1.getAnswer()]} ")
 
 
